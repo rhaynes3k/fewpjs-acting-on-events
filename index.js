876ec03faec2cc0e13987742ca9ece9e1ec28012
rhaynes3k <rhaynes3k@gmail.com>
2021-05-29 15:31:57 -0500

    done

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
 let dodger = document.getElementById('dodger');
 
 function moveDodgerLeft() {
-  var leftNumbers = dodger.style.left.replace('px', '');
-  var left = parseInt(leftNumbers, 10)
+  let leftNumbers = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumbers, 10)
   if (left > 0) {
     dodger.style.left = `${left - 1}px`
   }
 }
 
 function moveDodgerRight() {
-  var leftNumbers = dodger.style.left.replace('px', '');
-  var left = parseInt(leftNumbers, 10)
+  let leftNumbers = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumbers, 10)
   if (left > 0) {
     dodger.style.left = `${left + 1}px`
   }
